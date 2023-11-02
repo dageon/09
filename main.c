4c70af6a8bee338e9ae840d28ff71d19155967a0
dageon <93782184+dageon@users.noreply.github.com>
2023-11-02 16:15:29 +0900

    2-2

--- main.c
+++ main.c
@@ -6,14 +6,19 @@
 int main(int argc, char *argv[])
 {	
 	int grade[N_STUDENT];
-	int i;
+	int i, average;
+	int sum;
 	printf("input 5 scores: ");
 	
-	for(i=0;i<N_STUDENT;i++)
+	sum = 0;
+	for(i=0;i<N_STUDENT;i++) {
 		scanf("%d", &grade[i]);
+		sum += grade[i];
+	}
 	
-	for(i=0;i<N_STUDENT;i++)
-		printf("grade[%i] = %d\n", i, grade[i]);
+	average = sum / N_STUDENT;
+	
+	printf("score average : %i\n", average);
 	
 	return 0;
 }
